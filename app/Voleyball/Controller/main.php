9f60993b85ac61194fca84d404277df27900dcf9
supme <supmea@gmail.com>
2017-03-24 15:23:48 +0300

    Voleyball

---
+++ app/Voleyball/Controller/main.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package ly.
+ * @author Supme
+ * @copyright Supme 2014
+ * @license http://opensource.org/licenses/MIT MIT License	
+ *
+ *  THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF
+ *	ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ *	PURPOSE.
+ *
+ *	Please see the license.txt file for more information.
+ *
+ */
+
+namespace App\Voleyball\Controller;
+
+
+class main extends \Controller
+{
+    function index($params)
+    {
+        $data = new \App\Voleyball\Model\data();
+
+        $dateNow = new \DateTime();
+        $diff = date("w", $dateNow->getTimestamp());
+        $diff = $diff == 0?6:$diff-1;
+        $dateMon = new \DateTime();
+        $dateMon->sub(\DateInterval::createFromDateString($diff.' days'));
+        $dateSat = new \DateTime();
+        $dateSat->sub(\DateInterval::createFromDateString($diff.' days'));
+        $dateSat->add(\DateInterval::createFromDateString('6 days'));
+
+        \Registry::css([
+            "/assets/bootstrap/3.1.1/css/bootstrap.min.css",
+            "/assets/select2/css/select2.min.css",
+        ]);
+
+        \Registry::js([
+            "/assets/jquery/jquery-2.1.3.min.js",
+            "/assets/bootstrap/3.1.1/js/bootstrap.min.js",
+        ]);
+
+        $user = new \Auth();
+        $peoples = $data->people($dateMon->getTimestamp(), $dateSat->getTimestamp());
+        $status = $data->status($user->getUserId(), $dateMon->getTimestamp(), $dateSat->getTimestamp());
+        if (isset($_REQUEST['get']) /*and $user->isLogin*/) {
+            if ($_REQUEST['get'] == "people") {
+                $this->json($peoples);
+                return;
+            }
+            if ($_REQUEST['get'] == "status") {
+                $this->json(["status" => $status]);
+                return;
+            }
+
+        }
+        if (isset($_REQUEST['set']) /*and $user->isLogin*/) {
+            if ($_REQUEST['set'] == "on") {
+                $res = $data->set($user->getUserId());
+                $this->json(["status" => $res]);
+                return;
+            }
+            if ($_REQUEST['set'] == "off") {
+                $res = $data->del($user->getUserId(), $dateMon->getTimestamp(), $dateSat->getTimestamp());
+                $this->json(["status" => $res]);
+                return;
+            }
+        }
+
+        $this->render([
+            'peoples' => $peoples,
+            'check' => $status,
+        ]);
+    }
+}
---
+++ app/Voleyball/Model/data.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package ly.
+ * @author Supme
+ * @copyright Supme 2014
+ * @license http://opensource.org/licenses/MIT MIT License	
+ *
+ *  THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF
+ *	ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ *	PURPOSE.
+ *
+ *	Please see the license.txt file for more information.
+ *
+ */
+
+namespace App\Voleyball\Model;
+
+
+class data extends \Db {
+
+    function people($begin, $end) {
+        $data = $this->select(
+            'voleyball',
+            [  '[>]core_auth_user' => ['user_id' => 'id']],
+            ['core_auth_user.name'],
+            ['date[<>]' => [$begin, $end]]
+        );
+        return $data;
+    }
+
+    function status($user, $begin, $end) {
+        return $this->count('voleyball', ['AND' => ['date[<>]' => [$begin, $end], 'user_id' => $user]]) != 0;
+    }
+
+    function set($user) {
+        //insert
+        return $this->insert('voleyball',
+            [
+                'user_id' =>  $user,
+                'date' =>  time(),
+            ]
+        );
+    }
+
+    function del($user, $begin, $end) {
+        //delete
+        $this->delete('voleyball', ['AND' => ['date[<>]' => [$begin, $end], 'user_id' => $user]]);
+        return ;
+    }
+
+}
